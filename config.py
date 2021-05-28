eccadad2c011f8ca13a371a15ec868e73d5e2df0
Le.Beta <ccsmsblocker@gmail.com>
2021-05-28 09:02:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@
 DEFAULT_HEADERS = {
         'Accept': '*/*',
         'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
         'Connection': 'keep-alive',
         'Host': 'zhiyou.smzdm.com',
         'Referer': 'https://www.smzdm.com/',
         'Sec-Fetch-Dest': 'script',
         'Sec-Fetch-Mode': 'no-cors',
         'Sec-Fetch-Site': 'same-site',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
         }
 
 
